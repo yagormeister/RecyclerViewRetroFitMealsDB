faf07af0f2904b8171d5a7937b104686a3e3717c
Yago <yagogonzalezamat@gmail.com>
2023-12-21 13:25:36 +0100

    reduccion de min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.retromeal"
-        minSdk = 33
-        targetSdk = 33
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

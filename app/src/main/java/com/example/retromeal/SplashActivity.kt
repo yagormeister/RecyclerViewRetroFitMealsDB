b3dcf2e18a438072eb89e73ea1005c458d76f7c5
Yago González <yagogonzalezamat@gmail.com>
2024-01-11 14:25:53 +0100

    splash_activity

--- app/src/main/java/com/example/retromeal/SplashActivity.kt
+++ app/src/main/java/com/example/retromeal/SplashActivity.kt
@@ -1,16 +1,25 @@
+package com.example.retromeal
+
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.retromeal.MainActivity
 import com.example.retromeal.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var receiver: BroadcastReceiver
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
@@ -18,13 +27,19 @@ class SplashActivity : AppCompatActivity() {
         // Define el BroadcastReceiver
         receiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
-                // Cierra SplashActivity cuando se recibe la señal
-                finish()
+                // Handler para introducir un retraso
+                Handler(Looper.getMainLooper()).postDelayed({
+                    // Cierra SplashActivity después del retraso
+                    finish()
+                }, 2000) // Retraso de 2000 milisegundos (2 segundos)
             }
         }
 
+
         // Registra el BroadcastReceiver
-        registerReceiver(receiver, IntentFilter("com.example.retromeal.DATA_LOADED"))
+        registerReceiver(receiver, IntentFilter("com.example.retromeal.DATA_LOADED"),
+            RECEIVER_NOT_EXPORTED
+        )
 
         // Inicia MainActivity
         startActivity(Intent(this, MainActivity::class.java))
